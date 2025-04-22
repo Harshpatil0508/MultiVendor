1bb12bfe8cfa7eda5af14df2c8014471ba3597e7
Harshpatil9090 <patilharshvardhan9090@gmail.com>
2025-04-22 12:43:07 +0530

    Vendor Registration

---
+++ vendor/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Vendor
+# Register your models here.
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ('user','vendor_name','is_approved','created_at')
+    list_display_links = ('user','vendor_name')
+    list_filter = ('is_approved',)
+    search_fields = ('vendor_name',)
+    # list_editable = ('is_approved',)
+    list_per_page = 10
+
+admin.site.register(Vendor,VendorAdmin)
